ab3d048b576135a1e7506d6545de91cb89dcbd7c
Duy Nguyen <duy.nguyen2412@protonmail.com>
2024-01-17 20:36:08 +0100

    improve code

--- day5/main.py
+++ day5/main.py
@@ -8,9 +8,12 @@ for i in range(0, 100):
     else:
         print(i)
 
-#Password Generator Project
+# Password Generator Project
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -21,11 +24,13 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 password = ""
 for i in range(0, nr_letters):
-    password += letters[random.randint(0, len(letters)-1)]
+    password += random.choice(letters)
+    # password += letters[random.randint(0, len(letters)-1)]
 for i in range(0, nr_numbers):
-    password += numbers[random.randint(0, len(numbers)-1)]
+    # password += numbers[random.randint(0, len(numbers)-1)]
+    password += random.choice(numbers)
 for i in range(0, nr_symbols):
-    password += symbols[random.randint(0, len(symbols)-1)]
+    # password += symbols[random.randint(0, len(symbols) - 1)]
+    password += random.choice(symbols)
 
 print(f" your super secure password is: {password}")
-
