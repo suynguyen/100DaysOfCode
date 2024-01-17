9458f06a79645a5a2cdbf503e3486bfc0de95e88
Duy Nguyen <duy.nguyen2412@protonmail.com>
2024-01-17 19:03:32 +0100

    day 4: random, array, rock-paper-scissors-game done

--- day4/main.py
+++ day4/main.py
@@ -1,19 +1,65 @@
-left_or_right = input("Welcome to Treasure Island. Your Mission is to find the treasure. left or right?\n4").lower()
-if left_or_right == "left":
-    swim_or_wait = input("Would you like to swim or wait?\n").lower()
-    if swim_or_wait == "wait":
-        door_color = input("Which door do you want to open? red, blue, yellow oder anything\n").lower()
-        if door_color == "red":
-            print("Burned by fire. game over")
-        elif door_color == "blue":
-            print("Eaten by beasts. Game over")
-        elif door_color == "yellow":
-            print("You win!")
-        else:
-            print("Game Over.")
-    else:
-        print("Attacked by trout. Game Over")
-else:
-    print("Fall into a hole. Game over")
+import random
 
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+# Write your code below this line 👇
+
+user_choose = int(input("What do you choose? Type 0 for Rock, 1 for paper or 2 for Scissors"))
+
+random = random.randint(0, 2)
+if random == 0:
+    print(rock)
+elif random == 1:
+    print(paper)
+elif random == 2:
+    print(scissors)
+else:
+    print("error")
+
+if user_choose == random:
+    print("even, so try again")
+
+print("Computer choose")
+user_choose_as_String = str(user_choose)
+computer_choose_as_String = str(random)
+choose = computer_choose_as_String + user_choose_as_String
+user_win = "\n You win"
+user_lose = "\n You lose"
+if choose == "01":
+    print(paper + user_win)
+elif choose == "02":
+    print(scissors + user_lose)
+elif choose == "10":
+    print(rock + user_lose)
+elif choose == "12":
+    print(scissors + user_win)
+elif choose == "20":
+    print(rock + user_win)
+elif choose == "21":
+    print(paper + user_lose)
 
